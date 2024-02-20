a863f77073570e708cad40adfe9ff1182a6a2150
cami111e <camiwilson99@gmail.com>
2024-02-19 17:54:29 -0700

    Added bottom navigation bar, fragments for each menu item in the nav bar, and connected them in MainActivity.

---
+++ app/src/main/java/com/example/eventplanner/NotificationsFragment.java
@@ -0,0 +1,27 @@
+package com.example.eventplanner;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * Represents the fragment that shows users notifications.
+ */
+public class NotificationsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        // inflate the layout for the notifications fragment
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        return view;
+
+    }
+}
