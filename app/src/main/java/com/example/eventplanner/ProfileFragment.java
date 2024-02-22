96c95d57f18c5569a803240908886442cf03521b
ssajid <ssajid@ualberta.ca>
2024-02-21 20:00:33 -0700

    user can edit profile details, but still not connected to the database

--- app/src/main/java/com/example/eventplanner/ProfileFragment.java
+++ app/src/main/java/com/example/eventplanner/ProfileFragment.java
@@ -1,12 +1,15 @@
 package com.example.eventplanner;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,7 +22,7 @@ public class ProfileFragment extends Fragment {
     ImageView profilePic;
     Button editPic;
     TextView userName, userContact, userHomepage;
-    Button editUserName, editUserContact, editUserHomepage;
+    Button editDetails;
     Button location;
 
 
@@ -33,11 +36,16 @@ public class ProfileFragment extends Fragment {
         //initializing a user to show user's details on the profile screen
         User user = new User(R.drawable.p, "A", "a@email.com","www.a.com", true);
 
+        //getting all the layout objects
         profilePic = view.findViewById(R.id.profilePic);
+        editPic = view.findViewById(R.id.editProfilePic);
         userName = view.findViewById(R.id.profileName);
         userContact = view.findViewById(R.id.profileContact);
         userHomepage = view.findViewById(R.id.profileHomepage);
-        location = view.findViewById(R.id.editLocation);
+        location = view.findViewById(R.id.location);
+        editDetails = view.findViewById(R.id.editProfile);
+
+        //setting layout objects to the User object's values
         userName.setText(userName.getText()+user.username);
         userContact.setText(userContact.getText()+user.usercontact);
         userHomepage.setText(userHomepage.getText()+user.userhomepage);
@@ -46,6 +54,66 @@ public class ProfileFragment extends Fragment {
         } else {
             location.setText("OFF");
         }
+
+        //listening for edit button
+        editDetails.setOnClickListener(new View.OnClickListener() {
+            EditText editName, editContact, editHomepage;
+            Button saveBtn;
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(getContext());
+                dialog.setContentView(R.layout.edit_dialog);
+                dialog.show();
+
+                editName = dialog.findViewById(R.id.editName);
+                editContact = dialog.findViewById(R.id.editContact);
+                editHomepage = dialog.findViewById(R.id.editHomepage);
+                saveBtn = dialog.findViewById(R.id.saveBtn);
+
+                saveBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String name = "";
+                        String contact = "";
+                        String homePage = "";
+
+                        //getting user input
+                        name = editName.getText().toString().trim();
+                        contact = editContact.getText().toString().trim();
+                        homePage = editHomepage.getText().toString().trim();
+
+                        //updating the user object and the textviews
+                        userName.setText(name);
+                        userContact.setText(contact);
+                        userHomepage.setText(homePage);
+
+                        user.setUsername(name);
+                        user.setUsercontact(contact);
+                        user.setUserhomepage(homePage);
+
+                        dialog.dismiss();
+                    }
+                });
+
+            }
+        });
+
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(user.location){
+                    location.setText("OFF");
+                    user.setLocation(false);
+                } else {
+                    location.setText("ON");
+                    user.setLocation(true);
+                }
+            }
+        });
+
+        //profile pic edit listener
+
+
 
         return view;
 
