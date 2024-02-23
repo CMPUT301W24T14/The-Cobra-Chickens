842510728e73b4947d4f681953efd0860cf86968
Priyanshu Pusola <pusola@ualberta.ca>
2024-02-23 03:09:36 -0700

    Update Notification.java

--- app/src/main/java/com/example/eventplanner/Notification.java
+++ app/src/main/java/com/example/eventplanner/Notification.java
@@ -1,51 +1,27 @@
 package com.example.eventplanner;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 public class Notification {
-    private String title; // The title of the notification
-    private String message; // The detailed message of the notification
-    private LocalDateTime dateTime; // The date and time when the notification was created or should be shown
-    private boolean read; // Flag to check if the notification has been read
+    private String title;
+    private String message;
+    private Date date;
+    private boolean read;
 
-    // Constructor
-    public Notification(String title, String message, LocalDateTime dateTime, boolean read) {
+    public Notification(String title, String message, Date date, boolean read) {
         this.title = title;
         this.message = message;
-        this.dateTime = dateTime;
+        this.date = date;
         this.read = read;
     }
 
-    // Getters and Setters
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public boolean isRead() {
-        return read;
-    }
-
-    public void setRead(boolean read) {
-        this.read = read;
-    }
+    // Getters and setters
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+    public Date getDate() { return date; }
+    public void setDate(Date date) { this.date = date; }
+    public boolean isRead() { return read; }
+    public void setRead(boolean read) { this.read = read; }
 }
