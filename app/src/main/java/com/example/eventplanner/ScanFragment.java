7d4ba60280bc45ae1f6da2218af6b7301cebb003
Josiah Korzan <josiahkorzan4@gmail.com>
2024-03-28 14:12:12 -0600

    fixed qr scanner so that it now scans by checking the checkin code and not the eventId

--- app/src/main/java/com/example/eventplanner/ScanFragment.java
+++ app/src/main/java/com/example/eventplanner/ScanFragment.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +19,11 @@ import androidx.lifecycle.ViewModelProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.journeyapps.barcodescanner.CompoundBarcodeView;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ScanFragment extends Fragment {
     private CompoundBarcodeView barcodeView;
@@ -44,29 +49,47 @@ public class ScanFragment extends Fragment {
             AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
             if (result.getResult() != null) {
                 String[] parts = String.valueOf(result.getResult()).split(":");
-                String eventId = parts[0];
+                String checkInCodeFromQR = parts[0];
                 String userId = auth.getCurrentUser().getUid();
 
-                db.collection("users").document(userId).get().addOnSuccessListener(documentSnapshot -> {
-                    if (documentSnapshot.exists()) {
-                        db.collection("events").document(eventId).update("checkedInUsers", FieldValue.arrayUnion(userId));
-                        db.collection("events").document(eventId).update("signedUpUsers", FieldValue.arrayUnion(userId));
-                        db.collection("users").document(userId).update("myEvents", FieldValue.arrayUnion(eventId));
-                        db.collection("users").document(userId).update("checkedInto", FieldValue.arrayUnion(eventId));
+                final String[] eventId = new String[1];
+                db.collection("events").whereEqualTo("checkInCode", checkInCodeFromQR)
+                        .get()
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    eventId[0] = document.getId();
+                                }
+                                Log.d("EVENT ID", eventId[0]);
+                                db.collection("users").document(userId).get().addOnSuccessListener(documentSnapshot -> {
+                                    if (documentSnapshot.exists() && eventId[0] != null) {
+                                        db.collection("events").document(eventId[0]).update("signedUpUsers", FieldValue.arrayUnion(userId));
+                                        db.collection("users").document(userId).update("myEvents", FieldValue.arrayUnion(eventId[0]));
 
-                        sharedViewModel.setEventUpdated(true);
+                                        db.collection("events").document(eventId[0]).update("checkedInUsers", FieldValue.arrayUnion(userId));
+                                        db.collection("users").document(userId).update("checkedInto", FieldValue.arrayUnion(eventId[0]));
 
-                        builder.setTitle("Success!");
-                        builder.setMessage("You have successfully signed up for the event!");
-                    } else {
-                        builder.setTitle("Failure!");
-                        builder.setMessage("User does not exist!");
-                    }
-                    builder.setPositiveButton("OK", (dialog, which) -> {
-                        dialog.dismiss();
-                        barcodeView.resume();
-                    }).show();
-                });
+                                        sharedViewModel.setEventUpdated(true);
+
+                                        builder.setTitle("Success!");
+                                        builder.setMessage("You have successfully signed up for the event!");
+                                    } else {
+                                        builder.setTitle("Failure!");
+                                        if (eventId[0] == null) {
+                                            builder.setMessage("Event Id is empty");
+                                        } else {
+                                            builder.setMessage("User does not exist!");
+                                        }
+                                    }
+                                    builder.setPositiveButton("OK", (dialog, which) -> {
+                                        dialog.dismiss();
+                                        barcodeView.resume();
+                                    }).show();
+                                });
+                            }
+                        });
+
+
             } else {
                 builder.setTitle("Failure!");
                 builder.setMessage("result is null!");
