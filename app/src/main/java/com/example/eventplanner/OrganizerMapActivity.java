5ab8605b0984baa5a10511800b2909d263a48471
Dan4C0D3 <danic@ualberta.ca>
2024-04-06 20:37:46 -0600

    Google maps semi-working?

--- app/src/main/java/com/example/eventplanner/OrganizerMapActivity.java
+++ app/src/main/java/com/example/eventplanner/OrganizerMapActivity.java
@@ -17,12 +17,14 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -31,6 +33,9 @@ import java.util.ArrayList;
 public class OrganizerMapActivity extends AppCompatActivity implements OnMapReadyCallback {
     private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
     private MapView eventMap;
+    private GoogleMap googleMap;
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -48,22 +53,81 @@ public class OrganizerMapActivity extends AppCompatActivity implements OnMapRead
         String[] permissions = {android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE};
         requestPermissionsIfNecessary(permissions);
 
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
         eventMap = findViewById(R.id.organizer_map);
-        eventMap.getMapAsync(this);
-        //SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-        //        .findFragmentById(R.id.map_fragment);
-        //mapFragment.getMapAsync(this);
-        //GeoPoint startPoint = new GeoPoint(53.5461, -113.4937);
+        eventMap.onCreate(savedInstanceState);
 
+        eventMap.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
         LatLng edmonton = new LatLng(53.5461, -113.4937);
         googleMap.addMarker(new MarkerOptions()
                 .position(edmonton)
-                .title("Marker in Sydney"));
+                .title("Marker in Edmonton"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(edmonton, 9));
+        eventMap.onResume();
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        eventMap.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        eventMap.onResume();
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        eventMap.onStart();
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        eventMap.onStop();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        eventMap.onPause();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        eventMap.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory(){
+        super.onLowMemory();
+        eventMap.onLowMemory();
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
