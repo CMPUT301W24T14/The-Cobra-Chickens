f16d1f8375b0a50b61740bd562d8ddad6d8e4ce5
Dan4C0D3 <danic@ualberta.ca>
2024-04-08 11:54:37 -0600

    More unit tests.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.material:material:1.4.0")
 
-
 }
 secrets {
     // Optionally specify a different file name containing your secrets.
