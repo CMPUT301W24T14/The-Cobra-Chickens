0ea77f65abacb9a79775ae5e524e7e57f21773c3
Josiah Korzan <josiahkorzan4@gmail.com>
2024-04-08 11:49:13 -0600

    fixed promo qr allowing user to sign up to event even if max attendees had been reached

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,7 @@ dependencies {
     implementation("com.google.firebase:firebase-messaging:23.0.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.material:material:1.4.0")
+    implementation("com.google.android.gms:play-services-maps:17.0.1")
 
 
 }
